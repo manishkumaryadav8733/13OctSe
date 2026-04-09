3b3885982d1c0554bcfa21508bb6ae5ff24b476f
Manish Kumar <yadavdn72@gmail.com>
2026-04-09 14:40:23 +0530

    module14

---
+++ module14/lab/module.py
@@ -0,0 +1,10 @@
+import math
+import random
+
+# Using math module
+print(math.sqrt(16))   # 4.0
+print(math.ceil(4.3))  # 5
+print(math.floor(4.7)) # 4
+
+# Using random module
+print(random.randint(1, 100))  # Random number between 1 and 100
