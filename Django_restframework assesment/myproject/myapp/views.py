cfb6295b82deddfdff5a21fccf270111b69ff27a
Manish Kumar <yadavdn72@gmail.com>
2026-08-15 09:46:04 +0530

    all

--- Django_restframework assesment/myproject/myapp/views.py
+++ Django_restframework assesment/myproject/myapp/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from myapp.models import *
-from rest_framework import viewsets
+from rest_framework import viewsets,filters
 from django.db import transaction
 from .serializers import DoctorSerializer
 
 class DoctorViewSet(viewsets.ModelViewSet):
     queryset = Doctor.objects.all()
     serializer_class = DoctorSerializer
+    serializer_class = DoctorSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['name', 'specialization']
+
+
 
     def perform_create(self, serializer):
         with transaction.atomic():
